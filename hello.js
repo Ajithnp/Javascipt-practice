c9c7e15b4447d30baa9ae8dbce2919defdbc0d97
Ajith <ajithnp1617@gmail.com>
2025-01-07 11:34:26 +0530

    factory function added

--- hello.js
+++ hello.js
@@ -12,3 +12,32 @@ class Car {
 
 const carOne = new Car('toyota',2024)
 carOne.getInfo()
+
+// pupe and impure function
+function pure(a,b){
+    return a+b
+}
+console.log(pure(5,4))
+console.log(pure(5,4))
+console.log(pure(5,4))
+let sum = 0
+function impure(a){
+ sum += a
+ return sum;
+}
+console.log(impure(5))
+console.log(impure(5))
+console.log(impure(5))
+
+
+//factory function,
+function factory(name, place, designation){
+    return{
+        name,
+        place,
+        designation
+    }
+}
+
+const obj1 = factory('ajith','kochi', 'software engeener')
+console.log(obj1)
