41c59fe97d39d60c280ac488decc43f66c90d8b0
Ajith <ajithnp1617@gmail.com>
2025-01-07 11:43:00 +0530

    constructor function added

--- hello.js
+++ hello.js
@@ -12,3 +12,19 @@ class Car {
 
 const carOne = new Car('toyota',2024)
 carOne.getInfo()
+
+// constructor function
+
+function Constructor(name, place, designation){
+    this.name= name,
+    this.place = place,
+    this.designation = designation,
+    
+    this.getInfo = function (){
+        console.log(this.name+" "+ this.place+" "+this.designation);
+        
+    }
+}
+
+const person1 = new Constructor('ajith', 'banglore', 'software engeener')
+person1.getInfo()
